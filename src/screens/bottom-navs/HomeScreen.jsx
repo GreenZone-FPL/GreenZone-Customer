155d7b0651c64911aadda61c5792a3869bf4cf20
Tanniiee <tannie1101999@gmail.com>
2025-01-08 18:13:00 +0700

    custom DeliveryBottun check curren location

--- src/screens/bottom-navs/HomeScreen.jsx
+++ src/screens/bottom-navs/HomeScreen.jsx
@@ -1,27 +1,58 @@
-import {
-  Image,
-  SafeAreaView,
-  View,
-  Text,
-  Button,
-  FlatList,
-  Dimensions,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
-import React, {useState} from 'react';
-import ScreenEnum from '../../constants/screenEnum';
-import HeaderWithBadge from '../../components/headers/HeaderWithBadge';
-import colors from '../../constants/color';
-import LightStatusBar from '../../components/status-bars/LightStatusBar';
+import Geolocation from '@react-native-community/geolocation';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Button, Dimensions, SafeAreaView, StyleSheet, View } from 'react-native';
+import DeliveryButton from '../../components/buttons/DeliveryButton';
 import CategoryMenu from '../../components/category/CategoryMenu';
+import DialogShippingMethod from '../../components/dialogs/DialogShippingMethod';
+import HeaderWithBadge from '../../components/headers/HeaderWithBadge';
+import LightStatusBar from '../../components/status-bars/LightStatusBar';
+import colors from '../../constants/color';
+import GLOBAL_KEYS from '../../constants/globalKeys';
 
 const {width} = Dimensions.get('window');
 
 const HomeScreen = props => {
-
-
   const {navigation} = props;
+  const [currentLocation, setCurrenLocation] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  // Hàm xử lý khi đóng dialog
+  const handleCloseDialog = () => {
+    setIsModalVisible(false);
+  };
+
+  // Hàm xử lý khi chọn phương thức giao hàng
+  const handleOptionSelect = option => {
+    setSelectedOption(option);
+    setIsModalVisible(false); // Đóng dialog sau khi chọn
+  };
+  useEffect(() => {
+    Geolocation.getCurrentPosition(position => {
+      console.log(position);
+      if (position.coords) {
+        reverseGeocode({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      }
+    });
+  }, []);
+
+  const reverseGeocode = async ({lat, long}) => {
+    const api = `https://revgeocode.search.hereapi.com/v1/revgeocode?at=${lat},${long}&lang=vi-VI&apikey=Q9zv9fPQ8xwTBc2UqcUkP32bXAR1_ZA-8wLk7tjgRWo`;
+
+    try {
+      const res = await axios(api);
+      if (res && res.status === 200 && res.data) {
+        const items = res.data.items;
+        setCurrenLocation(items[0]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,14 +60,30 @@ const HomeScreen = props => {
       <HeaderWithBadge
         title="Home"
         onBadgePress={() => {
-          navigation.navigate('ProductDetailSheet')
+          navigation.navigate('ProductDetailSheet');
         }}
         isHome={true}
       />
-      <Button title='Checkout Screen' onPress={() => navigation.navigate('CheckoutScreen')}/>
+      <Button
+        title="Checkout Screen"
+        onPress={() => navigation.navigate('CheckoutScreen')}
+      />
 
       <CategoryMenu />
 
+      <View style={styles.deliverybuttom}>
+        <DeliveryButton
+          title="Đi giao đến"
+          address={currentLocation && currentLocation.address.label}
+          onPress={() => setIsModalVisible(true)}
+        />
+        <DialogShippingMethod
+          isVisible={isModalVisible}
+          selectedOption={selectedOption}
+          onHide={handleCloseDialog}
+          onOptionSelect={handleOptionSelect}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -47,7 +94,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: colors.white
-  }
-
+    backgroundColor: colors.white,
+  },
+  deliverybuttom: {
+    paddingHorizontal: GLOBAL_KEYS.PADDING_DEFAULT,
+    justifyContent: 'flex-end',
+    flex: 1,
+  },
 });
