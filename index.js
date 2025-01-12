63aec614fb993002e3300f8b35800922a8942dbc
Tanniiee <tannie1101999@gmail.com>
2025-01-12 13:48:23 +0700

    commit

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import MembershipCard from './src/screens/member-ship/MemberShipCard';
-AppRegistry.registerComponent(appName, () => MembershipCard);
+AppRegistry.registerComponent(appName, () => App);
 
 
