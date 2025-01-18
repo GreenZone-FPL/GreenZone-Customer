a61121c8c195630022a234b76c17bf4ddc389c0f
Phong Nguyen <nguyenhongphong1010.02@gmail.com>
2025-01-18 14:55:05 +0700

    Create Banner.jsx

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {GLOBAL_KEYS, colors} from '../../constants';
+
+const Banner = () => {
+  return (
+    <View>
+      <Text>Banner</Text>
+    </View>
+  );
+};
+
+export default Banner;
