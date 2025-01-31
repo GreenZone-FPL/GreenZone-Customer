7e403f39851db361e69a6468bfde0e2a4ca5b74c
ngocdai99 <ngocdai99@gmail.com>
2025-01-31 15:38:24 +0700

    Fix Toggle View

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import {name as appName} from './app.json';
 import TestGorhom from './src/screens/notification/TestGorhom';
 import TestPanel from './src/components/bottom-sheets/TestPanel';
 import ToggleView from './src/components/bottom-sheets/ToggleView';
-AppRegistry.registerComponent(appName, () => ToggleView);
+AppRegistry.registerComponent(appName, () => App);
 
 
