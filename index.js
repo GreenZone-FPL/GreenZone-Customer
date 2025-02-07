83d7fc4001fecb8fcf103ad21e28ac23fad67bbb
Tanniiee <tannie1101999@gmail.com>
2025-02-07 20:12:41 +0700

    commit

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import App from './App';
 import { name as appName } from './app.json';
 import ChatScreen from './src/screens/order/ChatScreen';
 import OrderDetailScreen from './src/screens/order/OrderDetailScreen';
-AppRegistry.registerComponent(appName, () => App);
+import TestComponent from './src/screens/bottom-navs/TestComponent';
+AppRegistry.registerComponent(appName, () => TestComponent);
