47d8967812a8eda1487c5bbc1040023154de0d46
ngocdai99 <ngocdai99@gmail.com>
2025-02-13 14:33:38 +0700

    import Skeleton to CategoryMenu, import getAllCategoriesApi to OrderScreen

--- App.jsx
+++ App.jsx
@@ -20,23 +20,23 @@ import AuthStackScreen from './src/layouts/stacks/AuthStackScreen';
 const BaseStack = createNativeStackNavigator();
 
 function App() {
- 
+
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        {/* Truyền navigationRef vào NavigationContainer */}
+       
         <NavigationContainer>
           <BaseStack.Navigator screenOptions={{ headerShown: false }}>
-          
-          <BaseStack.Screen
-              name={AppGraph.AUTHENTICATION}
-              component={AuthStackScreen}
-            />
             <BaseStack.Screen
               name={MainGraph.graphName}
               component={MainNavigation}
             />
+            <BaseStack.Screen
+              name={AppGraph.AUTHENTICATION}
+              component={AuthStackScreen}
+            />
+
             <BaseStack.Screen
               name={AuthGraph.LoginScreen}
               component={LoginScreen}
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import SendOTPScreen from './src/screens/auth/SendOTPScreen';
+import MySkeleton from './src/components/category/MySkeleton';
 
 AppRegistry.registerComponent(appName, () => App);
