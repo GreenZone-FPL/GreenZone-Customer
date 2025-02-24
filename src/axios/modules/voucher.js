2e41897f2750131052943196fbe904f3a2d40e59
Phong Nguyen <nguyenhongphong1010.02@gmail.com>
2025-02-24 09:37:24 +0700

    create

---
+++ src/axios/modules/voucher.js
@@ -0,0 +1,12 @@
+import axiosInstance from '../axiosInstance';
+
+export const getAllVoucher = async () => {
+  try {
+    const response = await axiosInstance.get('/v1/voucher/all');
+
+    return response.data;
+  } catch (error) {
+    console.log('error:', error);
+    throw error;
+  }
+};
