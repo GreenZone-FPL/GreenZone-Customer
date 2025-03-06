e552a7dfa7fd3c0168e495c3be46c5ebef05e96b
Phong Nguyen <nguyenhongphong1010.02@gmail.com>
2025-03-06 13:12:20 +0700

    update call api account get code

--- src/components/barcode/BarcodeBwipjs.jsx
+++ src/components/barcode/BarcodeBwipjs.jsx
@@ -3,20 +3,39 @@ import {View, StyleSheet, Dimensions} from 'react-native';
 import {SvgXml} from 'react-native-svg';
 import bwipjs from '@bwip-js/react-native';
 import {GLOBAL_KEYS} from '../../constants';
+import {getProfile} from '../../axios/index';
+import {Ani_ModalLoading} from '../animations/Ani_ModalLoading';
 
 const width = Dimensions.get('window').width;
 
 const Barcodebwipjs = () => {
   const [barcodeSVG, setBarcodeSVG] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [text, setText] = useState();
 
-  const [text, setText] = useState('0912345678');
+  // Lấy profile
+  const feathProfile = async () => {
+    setLoading(true);
+    try {
+      const reponse = await getProfile();
+      setText(reponse.code);
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    feathProfile();
+  }, []);
 
   useEffect(() => {
     const generateBarcode = async () => {
       try {
         const svg = await bwipjs.toSVG({
           bcid: 'code128',
-          text: text,
+          text: text ? text : 'Please create a new account',
           scale: 1,
           height: 5,
           includetext: true,
@@ -30,11 +49,12 @@ const Barcodebwipjs = () => {
     };
 
     generateBarcode();
-  }, []);
+  }, [text]);
 
   return (
     <View style={styles.container}>
       {barcodeSVG && <SvgXml xml={barcodeSVG} width="100%" height="100%" />}
+      <Ani_ModalLoading loading={loading} />
     </View>
   );
 };
