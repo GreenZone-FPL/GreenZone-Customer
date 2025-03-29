87baeb9bf058cfd410077d4fb292c9c0f5e0ff20
ngocdai99 <ngocdai99@gmail.com>
2025-03-29 14:57:59 +0700

    fix authen

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -24,7 +24,7 @@ import {Toaster} from '../../utils';
 import {BackHandler} from 'react-native';
 
 const LoginScreen = ({route, navigation}) => {
-  const [phoneNumber, setPhoneNumber] = useState('0123456798');
+  const [phoneNumber, setPhoneNumber] = useState('0123456799');
   const [phoneNumberError, setPhoneNumberError] = useState(false);
   const [phoneNumberMessage, setPhoneNumberMessage] = useState('');
   const [loading, setLoading] = useState(false);
