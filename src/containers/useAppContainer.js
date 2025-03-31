30ce5f945b41c4653ae8f936315425eb933a5692
ngocdai99 <ngocdai99@gmail.com>
2025-03-31 09:57:08 +0700

    Fix log out

--- src/containers/useAppContainer.js
+++ src/containers/useAppContainer.js
@@ -18,6 +18,7 @@ export const useAppContainer = () => {
     cartDispatch,
     authDispatch,
     authState,
+    setAwaitingPayments
   } = useAppContext();
 
   const navigation = useNavigation();
@@ -158,6 +159,7 @@ export const useAppContainer = () => {
   const onLogout = async () => {
     // Xóa token khỏi AsyncStorage
     await AppAsyncStorage.clearAll();
+    setAwaitingPayments(null)
     await CartManager.updateOrderInfo(cartDispatch, cartInitialState);
     authDispatch({
       type: AuthActionTypes.LOGOUT,
