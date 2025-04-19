61c07ce1a887714cf2eff706b33fc278a78b1b60
ngocdai99 <ngocdai99@gmail.com>
2025-04-19 11:20:21 +0700

    fix select Address

--- src/screens/bottom-navs/OrderScreen.jsx
+++ src/screens/bottom-navs/OrderScreen.jsx
@@ -21,7 +21,6 @@ import {
 import { colors, GLOBAL_KEYS } from '../../constants';
 import { useAuthActions, useOrderContainer } from '../../containers';
 import { useAppContext } from '../../context/appContext';
-import useSaveLocation from '../../utils/useSaveLocation';
 
 
 const OrderScreen = () => {
@@ -52,7 +51,7 @@ const OrderScreen = () => {
     navigateSearchProduct
   } = useOrderContainer()
 
-  useSaveLocation()
+
 
   const { onNavigateLogin } = useAuthActions();
 
