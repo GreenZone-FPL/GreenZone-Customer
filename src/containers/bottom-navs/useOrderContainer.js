f39b617364f2802257994fe3e841bfc504e848e8
ngocdai99 <ngocdai99@gmail.com>
2025-04-21 03:54:39 +0700

    fix bug order screen

--- src/containers/bottom-navs/useOrderContainer.js
+++ src/containers/bottom-navs/useOrderContainer.js
@@ -178,7 +178,7 @@ export const useOrderContainer = () => {
 
     fetchCategories();
 
-    fetchData(getAllProducts, setAllProducts);
+    fetchData(getAllProducts, setAllProducts, setLoading);
   }, []);
 
   const navigateCheckOut = () => {
